92dc86410d645b75f616e276ef63f48733e19e6d
TithiBagadia <bagadiatithi@gmail.com>
2026-08-21 16:14:05 +0530

    --

--- Tithi/ONLY_RTL/FIFOasncasync_tb.v
+++ Tithi/ONLY_RTL/FIFOasncasync_tb.v
@@ -2,45 +2,45 @@
 
 module FIFOsyncasync_tb;
 
-parameter datasize = 8;
-parameter addressize = 2;
-parameter prog_full_thresh = 2;
-parameter prog_empty_thresh = 2;
+parameter datasize = 8;                         // FIFO data width
+parameter addressize = 2;                       // FIFO address width
+parameter prog_full_thresh = 2;                // Programmable full threshold
+parameter prog_empty_thresh = 2;               // Programmable empty threshold
 
-reg wclk_sync;
-reg rclk_sync;
+reg wclk_sync;                                  // Synchronous FIFO write clock
+reg rclk_sync;                                  // Synchronous FIFO read clock
 
-reg wclk_async;
-reg rclk_async;
+reg wclk_async;                                 // Asynchronous FIFO write clock
+reg rclk_async;                                 // Asynchronous FIFO read clock
 
-reg rst_sync;
-reg rst_async;
+reg rst_sync;                                   // Synchronous FIFO reset
+reg rst_async;                                  // Asynchronous FIFO reset
 
-reg winc_sync;
-reg rinc_sync;
-reg [datasize-1:0] wdata_sync;
+reg winc_sync;                                  // Synchronous FIFO write enable
+reg rinc_sync;                                  // Synchronous FIFO read enable
+reg [datasize-1:0] wdata_sync;                 // Synchronous FIFO write data
 
-reg winc_async;
-reg rinc_async;
-reg [datasize-1:0] wdata_async;
+reg winc_async;                                 // Asynchronous FIFO write enable
+reg rinc_async;                                 // Asynchronous FIFO read enable
+reg [datasize-1:0] wdata_async;                // Asynchronous FIFO write data
 
-wire [datasize-1:0] rdata_sync;
-wire wfull_sync;
-wire rempty_sync;
-wire almost_full_sync;
-wire almost_empty_sync;
-wire prog_full_sync;
-wire prog_empty_sync;
+wire [datasize-1:0] rdata_sync;                // Synchronous FIFO read data
+wire wfull_sync;                                // Synchronous FIFO full flag
+wire rempty_sync;                               // Synchronous FIFO empty flag
+wire almost_full_sync;                          // Synchronous FIFO almost-full flag
+wire almost_empty_sync;                         // Synchronous FIFO almost-empty flag
+wire prog_full_sync;                            // Synchronous FIFO programmable-full flag
+wire prog_empty_sync;                           // Synchronous FIFO programmable-empty flag
 
-wire [datasize-1:0] rdata_async;
-wire wfull_async;
-wire rempty_async;
-wire almost_full_async;
-wire almost_empty_async;
-wire prog_full_async;
-wire prog_empty_async;
+wire [datasize-1:0] rdata_async;               // Asynchronous FIFO read data
+wire wfull_async;                               // Asynchronous FIFO full flag
+wire rempty_async;                              // Asynchronous FIFO empty flag
+wire almost_full_async;                         // Asynchronous FIFO almost-full flag
+wire almost_empty_async;                        // Asynchronous FIFO almost-empty flag
+wire prog_full_async;                           // Asynchronous FIFO programmable-full flag
 
 
+// Instantiate synchronous FIFO
 FIFOsyncasync_top #(
 .datasize(datasize),
 .addressize(addressize),
@@ -64,6 +64,7 @@ FIFOsyncasync_top #(
 );
 
 
+// Instantiate asynchronous FIFO
 FIFOsyncasync_top #(
 .datasize(datasize),
 .addressize(addressize),
@@ -87,30 +88,39 @@ FIFOsyncasync_top #(
 );
 
 
+// Generate waveform dump file
 initial
 begin
 $dumpfile("FIFOsyncasync_tb.vcd");
 $dumpvars(0,FIFOsyncasync_tb);
 end
 
+
+// Generate synchronous FIFO write clock
 initial
 begin
 wclk_sync = 0;
 forever #5 wclk_sync = ~wclk_sync;
 end
 
+
+// Generate synchronous FIFO read clock
 initial
 begin
 rclk_sync = 0;
 forever #5 rclk_sync = ~rclk_sync;
 end
 
+
+// Generate asynchronous FIFO write clock
 initial
 begin
 wclk_async = 0;
 forever #5 wclk_async = ~wclk_async;
 end
 
+
+// Generate asynchronous FIFO read clock
 initial
 begin
 rclk_async = 0;
@@ -118,6 +128,7 @@ forever #7 rclk_async = ~rclk_async;
 end
 
 
+// Apply reset and perform FIFO write/read operations
 initial
 begin
 
@@ -137,6 +148,7 @@ rst_sync = 0;
 rst_async = 0;
 
 
+// Write data into synchronous FIFO
 winc_sync = 1;
 
 wdata_sync = 8'hA1;
@@ -158,6 +170,7 @@ wdata_sync = 8'hD4;
 winc_sync = 0;
 
 
+// Write data into asynchronous FIFO
 winc_async = 1;
 
 wdata_async = 8'h11;
@@ -179,6 +192,7 @@ wdata_async = 8'h44;
 winc_async = 0;
 
 
+// Allow synchronous FIFO data to become available for reading
 repeat(3) @(posedge rclk_sync);
 
 rinc_sync = 1;
@@ -188,6 +202,7 @@ repeat(4) @(posedge rclk_sync);
 rinc_sync = 0;
 
 
+// Allow asynchronous FIFO data to become available for reading
 repeat(4) @(posedge rclk_async);
 
 rinc_async = 1;
@@ -204,4 +219,3 @@ $finish;
 end
 
 endmodule
-
