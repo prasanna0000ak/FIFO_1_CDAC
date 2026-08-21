58c13b7d6c0d7d3608d7ab68db7e47e1f08ea2da
TithiBagadia <bagadiatithi@gmail.com>
2026-08-21 16:10:29 +0530

    Update fifi_memory.v

--- Tithi/ONLY_RTL/fifi_memory.v
+++ Tithi/ONLY_RTL/fifi_memory.v
@@ -1,20 +1,18 @@
-
 module fifi_memory #(
 parameter datasize=8,
 parameter addressize=4
-)(input wclk,
- input  rclk,
- input wen,
- input ren,
- input [datasize-1:0]wdata,
- input [addressize-1:0] waddr,
- input [addressize-1:0]raddr,
- output reg [datasize-1:0] rdata
- 
- 
-    );
-    reg[datasize-1:0] mem[0:(2**addressize)-1];
+)(input wclk,                                  // Write clock
+ input  rclk,                                  // Read clock
+ input  wen,                                   // Write enable
+ input  ren,                                   // Read enable
+ input [datasize-1:0]wdata,                   // Data to be written
+ input [addressize-1:0] waddr,                // Write address
+ input [addressize-1:0]raddr,                 // Read address
+ output reg [datasize-1:0] rdata              // Data read from memory
+ );
+reg[datasize-1:0] mem[0:(2**addressize)-1]; // FIFO memory array
    
+// Write data into memory when write enable is active
 always @(posedge wclk)
 begin
 if(wen)
@@ -24,6 +22,7 @@ mem[waddr] <= wdata;
 end
 end
     
+// Read data from memory when read enable is active
     always@(posedge rclk)
     begin
     if(ren)
